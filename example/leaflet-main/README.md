033c04aa22f7ccf11b923cb634caeb4bf46b57d4
Crescens <2e3crescens@gmail.com>
2024-11-25 17:50:17 +0800

    separation of concerns

---
+++ example/leaflet-main/README.md
@@ -0,0 +1,35 @@
+# Food Places I Visited
+
+**Industry Project 1**
+A site that saves food places bookmarks and has a search + plan your route function with Foursquare API.
+
+# Features
+
+- **Bookmark**: bookmark the food places you have visited (WIP)
+  - Database: store food places to JSON.
+  - Map: Show markers from saved food places.
+  - Route: Draw polyline according to shown bookmarks.
+- **Search**: Search Food places (Future)
+- **Route**: Plan your route (Far Future)
+
+### Bookmark Features
+
+- Display marker when clicked on individual entries
+- Can save individual entries into categories
+- Create, Read, Update, Delete categories
+- Display cluster of markers when categories clicked
+
+### Search Features
+
+- Draw search results from Foursquare API and display into a list
+- Displaymarkers of search results
+
+### Route Features
+
+- Mark and link markers into a route
+- Implement bus/mrt api to include time estimation and best route
+- Food Trail: create a community page that has users can contribute pre-planned routes to share.
+  - Description
+  - Mode of transport
+  - time
+  - Key food places or highlights
---
+++ example/leaflet-main/script.js
@@ -0,0 +1,126 @@
+// CONTROLLER: Receive data from data.js and display it onto index.html
+document.addEventListener("DOMContentLoaded", function() {
+  // connect to json database
+  main();
+});
+
+async function main(){
+  // START Setting up essential variables =======================================================================
+  const JAWG_ACCESS_TOKEN =
+    "890xUFmEiNigo8eh1cbOpyQ7he6o2aq2kmrqIM0Vc9Knqm1wWgmACjHwmqwKE1VK";
+  let singaporeCor = [1.39,103.80]; // Singapore latlng
+  let singaporeMap = L.map("map").setView(singaporeCor, 13); // Set the center point
+  // END Setting up essential variables =======================================================================
+
+  // Displaying Jawg Sunny map tile
+  displayJawgSunnyTile(singaporeMap, JAWG_ACCESS_TOKEN);
+
+  // Group creation
+  let cckGroup = displayCckCordinates();
+  let randomMarkerClusterGroup = addRandomMarkerClusters(singaporeMap, 100);
+  let polylineGroup = await createPolylines();
+
+  // START layering =======================================================================
+  // baseLayers: only 1 baseLayer can appear at a time
+  let baseLayers = {
+
+  }
+  // overlays: all selected overlays/groups will appear
+  let overlays = {
+    'CCK': cckGroup,
+    'Random Clusters': randomMarkerClusterGroup,
+    'Polyline': polylineGroup
+  }
+  // Add layers to the map
+  L.control.layers(baseLayers, overlays).addTo(singaporeMap);
+  // END layering =======================================================================
+}// END main() =======================================================================
+
+
+
+function displayJawgSunnyTile(map, JAWG_ACCESS_TOKEN){
+  var Jawg_Sunny = L.tileLayer(
+    "https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token={accessToken}",
+    {
+      attribution:
+        '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      minZoom: 1,
+      maxZoom: 22,
+      accessToken: JAWG_ACCESS_TOKEN,
+    }
+  );
+  Jawg_Sunny.addTo(map);
+}
+
+function displayCckCordinates(){
+  let cckCor = [1.384, 103.747];
+  let cckGroup = L.layerGroup();
+
+  // cck marker --> cckGroup
+  cckMarker = L.marker(cckCor);
+  cckMarker.bindPopup(`Choa Chu Kang`);
+  cckMarker.addTo(cckGroup);
+
+  // cck circle --> cckGroup
+  let cckCircle = L.circle(cckCor, {
+    color: "red",
+    fillColor: `orange`,
+    fillOpacity: 0.1,
+    radius: 500,
+  });
+  cckCircle.addTo(cckGroup);
+
+  return cckGroup
+}
+
+function getRandomLatLng(map) {
+  // get the boundaries of the map
+  let bounds =    map.getBounds();
+  let southWest = bounds.getSouthWest();
+  let northEast = bounds.getNorthEast();
+  let lngSpan =   northEast.lng - southWest.lng;
+  let latSpan =   northEast.lat - southWest.lat;
+
+  let randomLng = Math.random() * lngSpan + southWest.lng;
+  let randomLat = Math.random() * latSpan + southWest.lat;
+
+  return [ randomLat, randomLng,];
+}
+
+function addRandomMarkerClusters(map, markerNumber){
+  // Create random marker cluster group
+  let randomMarkerClusterGroup = L.layerGroup();
+  // Create grandom marker cluster
+  let randomMarkerClusterLayer = L.markerClusterGroup();
+  for(let i = 0; i < markerNumber; i++){
+    let pos = getRandomLatLng(map);
+    L.marker(pos).addTo(randomMarkerClusterLayer);
+  }
+  // add marker cluster to map
+  randomMarkerClusterLayer.addTo(randomMarkerClusterGroup);
+
+  return randomMarkerClusterGroup;
+}
+
+async function createPolylines(){
+  let polylineGroup = L.layerGroup();
+
+  // Compiled locations
+  let places = await getBookmarks();
+  console.log(places)
+
+  // marker creation for compiled locations
+  places.forEach(place=>{
+    // create marker for each place
+    marker = L.marker(place.latlng);
+    marker.bindPopup(place.name);
+    marker.addTo(polylineGroup);
+  });
+  // push each latlng into array
+  let latlngArray = places.map(place=>place.latlng);
+  // polyline creation
+  let polyline = L.polyline(latlngArray, {color:'red'})
+  polyline.addTo(polylineGroup);
+
+  return polylineGroup;
+}
